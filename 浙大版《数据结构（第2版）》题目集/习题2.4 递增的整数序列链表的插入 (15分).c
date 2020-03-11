bdeb842f75c9cd9c4f84caa227f18449550692a5
渊澄 <yuancheng314@163.com>
2020-03-11 22:22:46 +0800

    提交ZJU_DS 习题2.4 递增的整数序列链表的插入 (15分).c

---
+++ 浙大版《数据结构（第2版）》题目集/习题2.4 递增的整数序列链表的插入 (15分).c
@@ -0,0 +1,22 @@
+bool Insert( List L, ElementType X )
+{   if (L->Last==MAXSIZE-1) /*满了*/
+    {   return false;   } 
+    
+    for(int i=1;i<=L->Last;i++)
+    {   if(X==L->Data[i])   /*找到了*/
+        {   return false;   }
+
+        if(X>L->Data[i])    /*这个位置的数已经比X大了*/
+        {   for(int j=L->Last+1;j>=i;j--)/*移位*/
+            {   L->Data[j]=L->Data[j-1];    }   
+            L->Data[i-1]=X;/*插入X*/
+            L->Last++;
+            return true;
+        }
+    }
+    
+    L->Data[++L->Last]=X; /*X比L中的元素都小，插在最后*/
+    return true;
+
+
+}
