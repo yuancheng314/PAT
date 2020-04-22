ef5318a4747beff8b702f01739e3a909e469e635
渊澄 <yuancheng314@163.com>
2020-04-22 13:31:35 +0800

    Update 1021.cpp

--- Advanced/1021.cpp
+++ Advanced/1021.cpp
@@ -3,25 +3,25 @@
 #include<set>
 using namespace std;
 
-const int N=10001; //??????????,?????????,?????Init???,??N??????,?????????????
+const int N=10001; //一开始我使用了宏定义，然而发现如果使用宏，在后续构造Init函数时，形参N也会被替换掉，所以这种还是最好不要使用宏
 vector<vector<int > >  G;
-vector<int> temp; //temp????DFS????????,Ans????
+vector<int> temp; //temp临时存放DFS的最远结果点结果,Ans保存答案
 set<int> Ans;
-int vis[N]={0},maxheight=0; //isRoot?????????????????
+int vis[N]={0},maxheight=0; //isRoot记录每个节点是否为某个集合的根节点
 
 void DFS(int u,int height)
-{   if(height>maxheight) //??temp,maxheight
+{   if(height>maxheight) //更新temp,maxheight
     {   temp.clear();
         temp.push_back(u);
         maxheight=height;
     }
     else if(height==maxheight) 
         temp.push_back(u);
-    vis[u]=1; //??????
+    vis[u]=1; //更新访问标识
     int size=G[u].size();
     for(int i=0;i<size;i++)
     {
-        if(vis[G[u][i]]==0) //????????????,????,???????
+        if(vis[G[u][i]]==0) //一开始这句后面加了个分号，当场毙命，找了很久才发现
             DFS(G[u][i],height+1);
     }
 
@@ -43,7 +43,7 @@ int main()
     {
     	if(vis[i]==0)
     	{
-    		DFS(i,1); //??????????????Advanced/1013.cpp
+    		DFS(i,1); //这里关于图的深度优先可以参考Advanced/1013.cpp
     		if(i==1)
     		{
     			if(temp.size()!=0) s1=temp[0];
@@ -55,7 +55,7 @@ int main()
 	}
     
     if(count>=2)  printf("Error: %d components\n",count);
-    else //????????
+    else //联通分量只有一个
     {   
 		temp.clear();
 		maxheight=0;
